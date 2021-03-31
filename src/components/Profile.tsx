3fc7874d57c66f5a6dec540d46fa2687867a5456
Shveytser-A <Shveytser-A@gaz-is.ru>
2021-03-31 13:49:48 +0300

    init App, Header, Navbar, Profile components

---
+++ src/components/Profile.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Profile() {
+    return <div className={'content'}>
+        <img src={'https://www.rgo.ru/sites/default/files/styles/head_image_article/public/odna_iz_konkursnyh_rabot_2011_goda._ogonkovyy_miting._v.a._masoshin.jpg?itok=xsNrqUux'}></img>
+        <div>
+            ava + desc
+        </div>
+        <div>
+            My posts
+            <div>
+                New post
+                <div>Post1</div>
+                <div>Post2</div>
+                <div>Post3</div>
+            </div>
+        </div>
+    </div>
+}
+
+export default Profile;
